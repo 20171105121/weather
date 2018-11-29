77a6181d9c13da811bb3451b2cee9e80269e5edc
20171105121 <s20171105121@mac24.local>
2018-11-29 08:29:10 +0800

    Initial Commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by cxm on 2018/11/29.
+//  Copyright © 2018 cxm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
